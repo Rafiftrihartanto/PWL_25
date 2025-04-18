5de74dcb0957d8348e929e8d2b28a69b839d2fd8
Rafif Tri Hartanto <rafiftrihartanto900@gmail.com>
2025-04-18 14:47:41 +0700

    js 2 praktikum

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController; 
+use App\Http\Controllers\PhotoController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/greeting', [WelcomeController::class, 
+'greeting']); 
+
+Route::resource('photos', PhotoController::class)->only([ 
+    'index', 'show' 
+]); 
+
+Route::resource('photos', PhotoController::class)->except([ 
+    'create', 'store', 'update', 'destroy' 
+]);
+
+Route::get('/hello', [WelcomeController::class,'hello']); 
+
+Route::get('/world', function () { 
+    return 'World'; 
+});
+
+Route::get('/user/{name}', function ($name) { 
+    return 'Nama saya '.$name; 
+});
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) { 
+return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+}); 
+
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name; 
 });
