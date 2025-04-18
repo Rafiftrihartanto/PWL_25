94933e2416683a8b8c78acb00600ef5f649a7beb
Rafif Tri Hartanto <rafiftrihartanto900@gmail.com>
2025-04-18 15:01:27 +0700

    js2 praktikum

---
+++ PWL_POS/routes/web.php
@@ -0,0 +1,15 @@
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::prefix('category')->group(function () {
+    Route::get('{slug}', [ProductController::class, 'show']);
+});
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+
+Route::get('/sales', [SalesController::class, 'index']);
