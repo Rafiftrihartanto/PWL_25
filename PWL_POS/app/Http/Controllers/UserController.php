557c04fe2240a3a07ad6b6ad62451e3d50f8393e
Rafif Tri Hartanto <rafiftrihartanto900@gmail.com>
2025-04-20 12:25:03 +0700

    js 4 praktikum 1

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,17 +10,19 @@ class UserController extends Controller
     public function index() {
         //tambah data user dengan Eloquent Model
         /*$data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
+            'level_id'
+            'username' => 'manager_dua',
+            'nama' => 'Manager 2',
             'password' => Hash::make('12345'),
-            'level_id' => 1
-        ];
-        UserModel::insert($data); // tambahkan data ke tabel m_users*/
+        ]; */
 
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data users
+        UserModel::create($data);
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_users
