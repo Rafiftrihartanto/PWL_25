a37740b1bfd5948c4a01602859e8d7a99520badf
Rafif Tri Hartanto <rafiftrihartanto900@gmail.com>
2025-04-20 12:14:29 +0700

    js 3 praktikum 6

---
+++ PWL_POS/app/Models/LevelModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class LevelModel extends Model
+{
+    use HasFactory;
+
+    //protected $fillable = ['level_id']; // Pastikan sesuai dengan kolom yang ada di database
+
+}
