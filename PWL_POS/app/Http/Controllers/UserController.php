1c869e43289660ff588d39451babd44c4ae17d42
Rafif Tri Hartanto <rafiftrihartanto900@gmail.com>
2025-04-21 11:58:48 +0700

    js 4 praktikum 2.3

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -33,9 +33,15 @@ class UserController extends Controller
             abort(404);
         }); 
 
-        $user = UserModel::findOrFail(1);*/
+        $user = UserModel::findOrFail(1);
 
         $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $user]);
+
+        $user = UserModel::where('level_id', 2)->count();
+        dd($user);
+        return view('user', ['data' => $user]);*/
+
+        $count = UserModel::where('level_id', 2)->count(); // hitung user dengan level 2
+        return view('user', ['count' => $count]); // kirim ke view
     }
 }
