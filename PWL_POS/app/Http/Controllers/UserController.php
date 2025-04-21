66a509c304b827adaa26a22b79b9661868a15162
Rafif Tri Hartanto <rafiftrihartanto900@gmail.com>
2025-04-21 11:45:46 +0700

    js 4 praktikum 2.2

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -27,11 +27,15 @@ class UserController extends Controller
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_users 
 
-        $user = UserModel::firstWhere('level_id', 1); */
+        $user = UserModel::firstWhere('level_id', 1); 
 
         $user = UserModel::findOr(20, ['username', 'nama'], function () {
             abort(404);
-        });
+        }); 
+
+        $user = UserModel::findOrFail(1);*/
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
