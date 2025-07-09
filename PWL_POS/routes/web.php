e9436c64c255ad85f4c9a8e0d0abb85a979e1d0f
Rafif Tri Hartanto <rafiftrihartanto900@gmail.com>
2025-07-09 13:00:47 +0700

    jobsheet 8 praktikum 2

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -41,7 +41,8 @@ Route::middleware('auth')->group(function () {
             Route::delete('/{id}/delete_ajax',[BarangController::class, 'delete_ajax']);
             Route::get('/import', [BarangController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
-
+            Route::get('/export_excel', [BarangController::class, 'export_excel']); // export excel
+            Route::get('/export_pdf', [BarangController::class, 'export_pdf']); // export pdf
         });
         
         Route::group(['prefix' => 'user'], function () {
@@ -55,7 +56,8 @@ Route::middleware('auth')->group(function () {
             Route::delete('/{id}/delete_ajax',[UserController::class, 'delete_ajax']);
             Route::get('/import', [UserController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [UserController::class, 'import_ajax']); // ajax import excel
-
+            Route::get('/export_excel', [UserController::class, 'export_excel']); // export excel
+            Route::get('/export_pdf', [UserController::class, 'export_pdf']); // export pdf
         });
         
         Route::group(['prefix' => 'level'], function () {
@@ -69,7 +71,8 @@ Route::middleware('auth')->group(function () {
             Route::delete('/{id}/delete_ajax',[LevelController::class, 'delete_ajax']);
             Route::get('/import', [LevelController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [LevelController::class, 'import_ajax']); // ajax import excel
-
+            Route::get('/export_excel', [LevelController::class, 'export_excel']); // export excel
+            Route::get('/export_pdf', [LevelController::class, 'export_pdf']); // export pdf
         });
         
         Route::group(['prefix' => 'supplier'], function () {
@@ -81,7 +84,8 @@ Route::middleware('auth')->group(function () {
             Route::delete('/{id}/delete_ajax',[SupplierController::class, 'delete_ajax']);
             Route::get('/import', [SupplierController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [SupplierController::class, 'import_ajax']); // ajax import excel
-
+            Route::get('/export_excel', [SupplierController::class, 'export_excel']); // export excel
+            Route::get('/export_pdf', [SupplierController::class, 'export_pdf']); // export pdf
         });
         
         Route::group(['prefix' => 'kategori'], function () {
@@ -93,7 +97,8 @@ Route::middleware('auth')->group(function () {
             Route::delete('/{id}/delete_ajax',[KategoriController::class, 'delete_ajax']);
             Route::get('/import', [KategoriController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [KategoriController::class, 'import_ajax']); // ajax import excel
-
+            Route::get('/export_excel', [KategoriController::class, 'export_excel']); // export excel
+            Route::get('/export_pdf', [KategoriController::class, 'export_pdf']); // export pdf
         });
     });
     
